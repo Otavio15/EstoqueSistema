1db70037f0bd1097e1e7c785bfb0a1b8e34b283f
otavio15 <otavio-roch@hotmail.com>
2021-02-15 21:20:06 -0300

    OK

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,17 @@
 # admin.py (ForeignKey)
 from django.contrib import admin
-from .models import CadastrarProduto, Cliente, CadastrarVeiculo, Produto, Categoria, Marca, Responsavel, ModeloVeiculo, Veiculo, Manutencao, Fabricante, Local, Abastecimento, Posto, TipoServico, UnidadeMedida
+from .models import CadastrarProduto, Cliente, CadastrarVeiculo, Produto, Categoria, Marca, Responsavel, ModeloVeiculo, Veiculo, Manutencao, Fabricante, Local, Abastecimento, Posto, UnidadeMedida, TipoServico
 
-class ProdutoInline(admin.TabularInline):
-    model = CadastrarProduto
+class TipoServicoInline(admin.TabularInline):
+    model = TipoServico
+    extra = 1
 
-class Project(admin.ModelAdmin):
-    inlines = [ProdutoInline]
-    class Meta:
-        model = Produto
-
+class ManutencaoAdmin(admin.ModelAdmin):
+    inlines = [TipoServicoInline]
 
 class CadastrarVeiculoAdmin(admin.ModelAdmin):
     list_display = ('modelo_veiculo', 'placa_veiculo', 'ano_veicuo')
+
 
 admin.site.register(CadastrarVeiculo, CadastrarVeiculoAdmin)
 admin.site.register(CadastrarProduto)
@@ -22,11 +21,11 @@ admin.site.register(Marca)
 admin.site.register(Responsavel)
 admin.site.register(ModeloVeiculo)
 admin.site.register(Veiculo)
-admin.site.register(Manutencao)
+admin.site.register(Manutencao, ManutencaoAdmin)
+admin.site.register(TipoServico)
 admin.site.register(Fabricante)
 admin.site.register(Local)
 admin.site.register(Posto)
 admin.site.register(Abastecimento)
-admin.site.register(TipoServico)
 admin.site.register(UnidadeMedida)
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -189,30 +189,14 @@ class CadastrarVeiculo(models.Model):
 
     def __str__(self):
         return str(self.nome_veiculo) + ' -- ' + str(self.placa_veiculo)
-
-class TipoServico(models.Model):
-    id = models.AutoField(primary_key=True)
-    tipo_servico = models.CharField('Tipo do serviço', max_length=50)
-    valor = models.DecimalField('Valor', max_digits=100000, decimal_places=2)
-    descricao = models.TextField('Descrição', blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = 'Tipo de serviço'
-        verbose_name_plural = 'Tipos de serviços'
-
-    def __str__(self):
-        return self.tipo_servico + ' - R$: '+str(self.valor)
     
 
 class Manutencao(models.Model):
     id = models.AutoField(primary_key=True)
-    veiculo = models.OneToOneField(CadastrarVeiculo,on_delete=models.CASCADE)
+    veiculo = models.OneToOneField(CadastrarVeiculo,on_delete=models.PROTECT)
     valor = models.DecimalField('Valor da manutenção', max_digits=100000, decimal_places=2)
     data = models.DateField('Data da manutenção')
-    local = models.OneToOneField(Local, on_delete=models.CASCADE, verbose_name='Local da manutenção')
-    tipo_servico = models.ManyToManyField(TipoServico)
+    local = models.OneToOneField(Local, on_delete=models.PROTECT, verbose_name='Local da manutenção')
     descricao = models.TextField('Descrição', blank=True)
     imagem = models.ImageField('Imagem',upload_to='img', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -225,6 +209,19 @@ class Manutencao(models.Model):
     def __str__(self):
         return 'Veículo: '+str(self.veiculo) +' - Valor: '+str(self.valor)+' - Local: '+str(self.local)
 
+class TipoServico(models.Model):
+    tipo_servico = models.CharField('Serviço', max_length=50)
+    valor = models.DecimalField('Valor do serviço', max_digits=100000, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    manutencao = models.ForeignKey(Manutencao, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Tipo de serviço'
+        verbose_name_plural = 'Tipos de serviços'
+
+    def __str__(self):
+        return self.tipo_servico + ' - R$: '+str(self.valor)
 
 class Posto(models.Model):
     id = models.AutoField(primary_key=True)
