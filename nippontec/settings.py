3ba139e9e052f5c129b1265311bce77ef39b4f72
otavio15 <otavio-roch@hotmail.com>
2021-03-09 14:33:12 -0300

    ok

--- nippontec/settings.py
+++ nippontec/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'nippontec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estoque',
+        'USER': 'pi',
+        'PASSWORD': 'su2141po',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
